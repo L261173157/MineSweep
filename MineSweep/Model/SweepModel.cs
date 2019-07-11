cde95d572efb5d03fd8699bf4507bcfc838cd4a9
猫猫秋小裤 <261173157@qq.com>
2019-07-11 16:56:35 +0800

    添加项目文件。

---
+++ MineSweep/Model/SweepModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace MineSweep.Model
+{
+    public class SweepModel: ObservableObject
+    {
+        private int[,] sweep;
+
+        public int[,] Sweep { get => sweep; set { sweep = value; RaisePropertyChanged(() => Sweep); } }
+    }
+}
---
+++ MineSweep/ViewModel/SweepViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using MineSweep.Model;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.Messaging;
+
+
+namespace MineSweep.ViewModel
+{
+   public class SweepViewModel: ViewModelBase
+    {
+        public SweepViewModel()
+        {
+            sweepModel = new SweepModel() { Sweep = new int[5, 5] };
+        }
+        #region 属性
+        private SweepModel sweepModel;
+        public SweepModel SweepModel
+        {
+            get
+            {
+                return sweepModel;
+            }
+            set
+            {
+                sweepModel = value;
+                RaisePropertyChanged(nameof(SweepModel));
+            }
+        }
+        #endregion
+    }
+}
