c545652f43b49266568b7332365061e0552e26fb
猫猫秋小裤 <261173157@qq.com>
2019-07-12 17:01:02 +0800

    0712

--- MineSweep/Model/SweepModel.cs
+++ MineSweep/Model/SweepModel.cs
@@ -20,9 +20,20 @@ namespace MineSweep.Model
 
        
 
-        private List<int> sweepView1D;
+        private List<int> sweepView1D; //页面显示数据的一维输出，用于wpf显示
         public List<int> SweepView1D { get => sweepView1D; set { sweepView1D = value; RaisePropertyChanged(() => SweepView1D); } }
 
+        private int sweepNumer;  //地雷数据
+
+        public int SweepNumer { get => sweepNumer; set { sweepNumer = value; RaisePropertyChanged(() => SweepNumer); } }
         #endregion
+    }
+    public class SweepViewParam
+    {
+        private int locationX;  //按钮x位置
+        private int locationY;  //按钮x位置
+
+        public int LocationX { get => locationX; set => locationX = value; }
+        public int LocationY { get => locationY; set => locationY = value; }
     }
 }
--- MineSweep/ViewModel/SweepViewModel.cs
+++ MineSweep/ViewModel/SweepViewModel.cs
@@ -16,7 +16,7 @@ namespace MineSweep.ViewModel
     {
         public SweepViewModel()
         {
-            sweepModel = new SweepModel() { Sweep = new int[5, 5], SweepView = new int[5, 5], SweepView1D = new List<int>() };
+            sweepModel = new SweepModel() { Sweep = new int[5, 5], SweepView = new int[5, 5], SweepView1D = new List<int>(),SweepNumer=5 };
         }
         #region 属性
         private SweepModel sweepModel;
@@ -32,6 +32,21 @@ namespace MineSweep.ViewModel
                 RaisePropertyChanged(nameof(SweepModel));
             }
         }
+        //界面点击位置参数
+        private SweepViewParam sweepViewParam;
+        public SweepViewParam SweepViewParam
+        {
+            get
+            {
+                return sweepViewParam;
+            }
+            set
+            {
+                sweepViewParam = value;
+                RaisePropertyChanged(nameof(SweepViewParam));
+            }
+        }
+
         #endregion
 
         #region 命令
@@ -49,11 +64,66 @@ namespace MineSweep.ViewModel
                 testCmd = value;
             }
         }
+        //重新生成
+        private RelayCommand resetCmd;
+        public RelayCommand ResetCmd
+        {
+            get
+            {
+                if (resetCmd == null)
+                    return new RelayCommand(Reset);
+                return resetCmd;
+            }
+            set
+            {
+                resetCmd = value;
+            }
+        }
+        //点击命令
+        private RelayCommand<SweepViewParam> viewClick;
+        public RelayCommand<SweepViewParam> ViewClick
+        {
+            get
+            {
+                if (viewClick == null)
+                    return new RelayCommand<SweepViewParam>(Click);
+                return viewClick;
+            }
+            set
+            {
+                viewClick = value;
+            }
+        }
         #endregion
         #region 命令方法
-        private void Test()
+        //用于界面测试的方法
+        private void Test() 
         {
-            sweepModel.SweepView1D = sweepModel.SweepView.Cast<int>().ToList();
+            sweepModel.SweepView1D = sweepModel.Sweep.Cast<int>().ToList();
+        }
+        //重新生成地雷及界面
+        private void Reset()
+        {
+            Array.Clear(sweepModel.Sweep, 0, sweepModel.Sweep.Length);
+            Random random = new Random();
+            int i = 0;
+            while(true)
+            {
+                int a = random.Next(0, 5);
+                int b = random.Next(0, 5);
+                if(sweepModel.Sweep[a,b]==0)
+                {
+                    sweepModel.Sweep[a, b] = 1;
+                    i++;
+                }
+                if (i == sweepModel.SweepNumer)
+                    break;
+            }
+        }
+        //点击界面按钮事件方法
+        private void Click(SweepViewParam param)
+        {
+            sweepViewParam = param;
         }
 
         #endregion
